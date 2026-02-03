c89d47a143e05cfadc97743569259767bfab1a6f
viniciusmiranda022-pixel <vinicius.miranda022@gmail.com>
2026-02-03 20:21:44 -0300

    Exclude smoke test sources from app build

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,9 +1,1 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("DirectoryAnalyzer")]
-[assembly: AssemblyProduct("DirectoryAnalyzer")]
-[assembly: AssemblyCompany("DirectoryAnalyzer")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+// Assembly metadata is defined in DirectoryAnalyzer.csproj.
